75e07ea6e2ae514b9a6f1c1e7d074e95e813ae35
Xie Lin <xiel0211@gmail.com>
2018-12-09 18:04:15 -0500

    change

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function(){
+jQuery (document) . ready(function ($){
     $('#convertercm').on('submit', function(event){
         alert('Form Submission Intercepted!');
         event.preventDefault();
